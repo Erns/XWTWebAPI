56a34a54bbf3f70acd2e889155bcf0e0b4eef67d
aaron.sayles <aaron.sayles@gmail.com>
2018-09-16 00:37:01 -0500

    20180916

---
+++ XWTWebAPI/Controllers/TournamentsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace XWTWebAPI.Controllers
+{
+    public class TournamentsController : ApiController
+    {
+        // GET api/values
+        public string Get()
+        {
+            return "";
+        }
+
+        // GET api/values/5 (READ)
+        public string Get(int id)
+        {
+
+            return "";
+        }
+
+        // POST api/values (CREATE)
+        public string Post([FromBody]string value)
+        {
+            return "";
+        }
+
+        // PUT api/values/5 (UPDATE)
+        public string Put(int id, [FromBody]string value)
+        {
+            return "";
+        }
+
+        // DELETE api/values/5 (DELETE)
+        public void Delete(int id)
+        {
+        }
+
+    }
+}
--- XWTWebAPI/Controllers/UserAccountController.cs
+++ XWTWebAPI/Controllers/UserAccountController.cs
@@ -13,80 +13,126 @@ namespace XWTWebAPI.Controllers
 {
     public class UserAccountController : ApiController
     {
-        // GET api/values (READ)
-        public IEnumerable<string> Get()
+        //// GET api/values (READ)
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/values/5 (READ) (GET doesn't appear to accept [FromBody], so passing as a string instead)
+        public string Get(string value)
         {
-            return new string[] { "value1", "value2" };
-        }
+            try
+            {          
+                UserAccount result = JsonConvert.DeserializeObject<UserAccount>(JsonConvert.DeserializeObject(value).ToString());
 
-        // GET api/values/5 (READ)
-        public string Get([FromBody]string value)
-        {
-            UserAccount result = JsonConvert.DeserializeObject<UserAccount>(JsonConvert.DeserializeObject(value).ToString());
-
-            using (SqlConnection sqlConn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["XWTWebConnectionString"].ToString()))
-            {
-                sqlConn.Open();
-
-                //Check if user account exist
-                using (SqlCommand sqlCmd = new SqlCommand("dbo.spUserAccounts_VALIDATE", sqlConn))
+                using (SqlConnection sqlConn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["XWTWebConnectionString"].ToString()))
                 {
-                    sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
-                    sqlCmd.Parameters.AddWithValue("@UserName", result.UserName);
-                    sqlCmd.Parameters.AddWithValue("@Password", result.Password);
+                    sqlConn.Open();
 
-                    using (SqlDataReader sqlRdr = sqlCmd.ExecuteReader())
+                    int intUserId = 0;
+
+                    //Check if user account exist
+                    using (SqlCommand sqlCmd = new SqlCommand("dbo.spUserAccounts_VALIDATE", sqlConn))
                     {
-                        if (sqlRdr.HasRows)
+                        sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        sqlCmd.Parameters.AddWithValue("@UserName", result.UserName);
+                        sqlCmd.Parameters.AddWithValue("@Password", result.Password);
+
+                        SqlParameter outputParameter = new SqlParameter("@Id", SqlDbType.Int);
+                        outputParameter.Direction = ParameterDirection.Output;
+
+                        sqlCmd.Parameters.Add(outputParameter);
+
+                        using (SqlDataReader sqlRdr = sqlCmd.ExecuteReader())
                         {
-                            return "GET: TRUE";
+                            intUserId = Convert.ToInt32(outputParameter.Value);
                         }
                     }
-                }
 
+                    if (intUserId == 0)
+                    {
+                        return "GET: FALSE UserId = 0";
+                    }
+                    else
+                    {
+                        UserAccount user = new UserAccount();
+
+                        //Get the basic user info
+                        using (SqlCommand sqlCmd = new SqlCommand("dbo.spUserAccounts_GETBASIC", sqlConn))
+                        {
+                            sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
+                            sqlCmd.Parameters.AddWithValue("@Id", intUserId);
+                            sqlCmd.Parameters.AddWithValue("@Password", result.Password);
+
+                            using (SqlDataReader sqlRdr = sqlCmd.ExecuteReader())
+                            {
+                                if (sqlRdr.Read())
+                                {
+                                    user.Id = sqlRdr.GetInt32(sqlRdr.GetOrdinal("Id"));
+                                    user.UserName = sqlRdr.GetString(sqlRdr.GetOrdinal("UserName"));
+                                    user.Email = sqlRdr.GetString(sqlRdr.GetOrdinal("Email"));
+                                    return JsonConvert.SerializeObject(user);
+                                }
+                            }
+                        }
+                    }
+
+                }
+                return "GET: FALSE";
             }
-            return "GET: FALSE";
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
 
         // POST api/values (CREATE)
         public string Post([FromBody]string value)
         {
 
-            UserAccount result = JsonConvert.DeserializeObject<UserAccount>(JsonConvert.DeserializeObject(value).ToString());
+            try
+            {            
+                UserAccount result = JsonConvert.DeserializeObject<UserAccount>(JsonConvert.DeserializeObject(value).ToString());
 
-            using (SqlConnection sqlConn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["XWTWebConnectionString"].ToString()))
-            {
-                sqlConn.Open();
-
-                //Check if user account exist
-                using (SqlCommand sqlCmd = new SqlCommand("dbo.spUserAccounts_EXISTS", sqlConn))
+                using (SqlConnection sqlConn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["XWTWebConnectionString"].ToString()))
                 {
-                    sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
-                    sqlCmd.Parameters.AddWithValue("@UserName", result.UserName);
-                    sqlCmd.Parameters.AddWithValue("@Email", result.Email);
+                    sqlConn.Open();
 
-                    using (SqlDataReader sqlRdr = sqlCmd.ExecuteReader())
+                    //Check if user account exist
+                    using (SqlCommand sqlCmd = new SqlCommand("dbo.spUserAccounts_EXISTS", sqlConn))
                     {
-                        if (sqlRdr.HasRows)
+                        sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        sqlCmd.Parameters.AddWithValue("@UserName", result.UserName);
+                        sqlCmd.Parameters.AddWithValue("@Email", result.Email);
+
+                        using (SqlDataReader sqlRdr = sqlCmd.ExecuteReader())
                         {
-                            return "POST: User Account Already Exists!";
+                            if (sqlRdr.HasRows)
+                            {
+                                return "POST: User Account Already Exists!";
+                            }
                         }
+                    }
+
+                    //Create user
+                    using (SqlCommand sqlCmd = new SqlCommand("dbo.spUserAccounts_UPDATEINSERT", sqlConn))
+                    {
+                        sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        sqlCmd.Parameters.AddWithValue("@Id", "0");
+                        sqlCmd.Parameters.AddWithValue("@UserName", result.UserName);
+                        sqlCmd.Parameters.AddWithValue("@Password", result.Password); //Should already be hashed on the client side
+                        sqlCmd.Parameters.AddWithValue("@Email", result.Email);
+                        sqlCmd.ExecuteNonQuery();
                     }
                 }
 
-                //Create user
-                using (SqlCommand sqlCmd = new SqlCommand("dbo.spUserAccount_UPDATEINSERT", sqlConn))
-                {
-                    sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
-                    sqlCmd.Parameters.AddWithValue("@UserName", result.UserName);
-                    sqlCmd.Parameters.AddWithValue("@Password", result.Password); //Should already be hashed on the client side
-                    sqlCmd.Parameters.AddWithValue("@Email", result.Email);
-                    sqlCmd.ExecuteNonQuery();
-                }
+                return "POST: Success";
             }
-
-            return "POST: Success";
-
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
 
         // PUT api/values/5 (UPDATE)
